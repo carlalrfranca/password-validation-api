6168252e0604801296126846f4405c2f11d047ee
Carla Leticia Ribeiro Franca <carlainlondon@protonmail.com>
2026-02-16 17:27:33 -0300

    feat(domain): add HasSpecialCharRule with unit test

--- src/main/java/br/com/clrf/domain/rules/HasSpecialCharRule.java
+++ src/main/java/br/com/clrf/domain/rules/HasSpecialCharRule.java
@@ -1,4 +1,16 @@
 package br.com.clrf.domain.rules;
 
-public class HasSpecialCharRule {
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class HasSpecialCharRule implements PasswordRule {
+
+        private final String specialChars;
+
+        @Override
+        public boolean isSatisfiedBy(String password) {
+
+        return password.chars().anyMatch(c -> specialChars.indexOf(c) >= 0);
+
+    }
 }
--- src/test/java/br/com/clrf/domain/rules/HasSpecialCharRuleTest.java
+++ src/test/java/br/com/clrf/domain/rules/HasSpecialCharRuleTest.java
@@ -1,4 +1,43 @@
 package br.com.clrf.domain.rules;
 
-public class HasSpecialCharRuleTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HasSpecialCharRuleTest {
+
+    private static final String SPECIAL = "!@#$%^&*()-+";
+    private HasSpecialCharRule rule;
+    private List<String> invalid;
+    private List<String> valid;
+
+    @BeforeEach
+    void setUp() {
+        rule = new HasSpecialCharRule(SPECIAL);
+
+        invalid = List.of("AbTp9fok", "ABC123def", "a1B2c3");
+        valid = List.of("AbTp9!fok", "a1+", "A-9b");
+    }
+
+    @Test
+    void shouldReturnFalseWhenNoSpecialChar() {
+        for (String p : invalid) {
+            assertFalse(rule.isSatisfiedBy(p));
+        }
+    }
+
+    @Test
+    void shouldReturnTrueWhenHasAllowedSpecialChar() {
+        for (String p : valid) {
+            assertTrue(rule.isSatisfiedBy(p));
+        }
+    }
+
+    @Test
+    void shouldReturnFalseWhenHasAllowedSpecialChar() {
+        assertFalse(rule.isSatisfiedBy("abc?123"));
+    }
 }
