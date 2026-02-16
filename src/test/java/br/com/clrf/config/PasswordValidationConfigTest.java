82098b8673b1dd967a22330bb17c80e3cf8733cb
Carla Leticia Ribeiro Franca <carlainlondon@protonmail.com>
2026-02-16 19:18:27 -0300

    feat(config): add PaswordValidationConfig unit tests

---
+++ src/test/java/br/com/clrf/config/PasswordValidationConfigTest.java
@@ -0,0 +1,31 @@
+package br.com.clrf.config;
+
+import br.com.clrf.domain.policy.PasswordPolicy;
+import br.com.clrf.service.PasswordUseCase;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidationConfigTest {
+
+    @Test
+    void shouldLoadPasswordPolicyBean() {
+        try (var context = new AnnotationConfigApplicationContext(PasswordValidationConfig.class)) {
+
+            PasswordPolicy policy = context.getBean(PasswordPolicy.class);
+
+            assertNotNull(policy);
+        }
+    }
+
+    @Test
+    void shouldLoadPasswordUseCaseBean() {
+        try (var context = new AnnotationConfigApplicationContext(PasswordValidationConfig.class)) {
+
+            PasswordUseCase useCase = context.getBean(PasswordUseCase.class);
+
+            assertNotNull(useCase);
+        }
+    }
+}
---
+++ src/test/java/br/com/clrf/web/controller/PasswordControllerTest.java
@@ -0,0 +1,4 @@
+package br.com.clrf.web.controller;
+
+public class PasswordControllerTest {
+}
