594f4ae6f05a5e4a8ea1b4499610621712cc7d48
Carla Leticia Ribeiro Franca <carlainlondon@protonmail.com>
2026-02-17 15:39:39 -0300

    chore: remove temporary comments from controller and request

--- src/main/java/br/com/clrf/web/dto/PasswordRequest.java
+++ src/main/java/br/com/clrf/web/dto/PasswordRequest.java
@@ -4,8 +4,3 @@ import jakarta.validation.constraints.NotNull;
 
 public record PasswordRequest(
         @NotNull(message = "Password cannot be null")String password) {}
-
-
-//package br.com.clrf.web.dto;
-//
-//public record PasswordRequest(String password) {}
