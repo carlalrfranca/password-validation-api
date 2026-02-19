81d2fe0ba957ce295cceed3c888f3baebb12efad
carlalrfranca <71091956+carlalrfranca@users.noreply.github.com>
2026-02-19 09:34:12 -0300

    Remove commented code explaining Spring Boot annotations
    Removed commented explanation about Spring Boot annotations.

--- src/main/java/br/com/clrf/PasswordValidationApplication.java
+++ src/main/java/br/com/clrf/PasswordValidationApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//COMBINA @Configuration (permite BEAN como FONTE de configuração, @EnableAutoConfiguration (MVC e Tomcat)e @ComponentScan (busca classes anotadas para registrr como beans)
 class PasswordValidationApplication {
 
     public static void main(String[] args) {
